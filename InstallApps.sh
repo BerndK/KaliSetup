d761a25f5c973f0acc5d3fbfe02e90a9d884d5fc
BerndK <bk@bkedv.de>
2021-09-22 16:54:11 +0200

    added some tools

--- InstallApps.sh
+++ InstallApps.sh
@@ -14,8 +14,16 @@ echo "#########################################"
 # hardinfo          - Hardware Information Tool
 # kali-tools-802-11 - Kali WiFi Tools
 # tilix             - Multi terminal
+# docker-compose    - for docker containers
+# xarchiver         - GUI to handle archives
+# johnny            - GUI to crack passwords
 sudo apt update
-sudo apt install searchmonkey bleachbit duc ksysguard armitage hardinfo kali-tools-802-11 tilix docker-compose -y
+sudo apt install searchmonkey bleachbit duc ksysguard armitage hardinfo kali-tools-802-11 tilix docker-compose xarchiver johnny -y
 
 # Remove invalid exploit (to not break armitage):
 sudo mv /usr/share/metasploit-framework/modules/exploits/linux/misc/saltstack_salt_unauth_rce.rb /usr/share/metasploit-framework/modules/exploits/linux/misc/saltstack_salt_unauth_rce.rb.bkp
+
+# open cl for hashcat
+# not tested yet, hascat seems to not work in VM
+# sudo apt install libhwloc-dev ocl-icd-dev ocl-icd-opencl-dev
+# sudo apt install pocl-opencl-icd
