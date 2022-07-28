8ee49a1078d3cf60820bb089e8fe1b4b178fc8e6
BerndK <bk@bkedv.de>
2022-07-28 17:25:23 +0200

    Pyrit installation: use Python2

--- WifiteTools.sh
+++ WifiteTools.sh
@@ -11,13 +11,13 @@ sudo apt install hcxdumptool hcxtools python3-pip cowpatty -y
 sudo pip install psycopg2
 sudo pip install scapy
 
-sudo apt install python-dev libssl-dev libpcap-dev -y
+sudo apt install python2-dev libssl-dev libpcap-dev -y
 
 git clone https://github.com/JPaulMora/Pyrit.git
 cd Pyrit
-python setup.py clean
-python setup.py build  
-sudo python setup.py install
+python2 setup.py clean
+python2 setup.py build  
+sudo python2 setup.py install
 #cleanup
 cd ..
 rm Pyrit -rf
